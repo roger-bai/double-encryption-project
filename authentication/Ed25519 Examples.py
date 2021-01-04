e5d96e2ac2d574f296b857b47263426283298983
Roger Bai <69400368+roger-bai@users.noreply.github.com>
2021-01-04 17:47:31 -0500

    Create Ed25519 Examples.py

---
+++ authentication/Ed25519 Examples.py
@@ -0,0 +1,21 @@
+pip install ed25519
+import ed25519
+
+# Create the public and private keys.
+
+privkey, pubkey = ed25519.create_keypair()
+
+# To see what the keys look like.
+print("Private Key (32 bytes):", privkey.to_ascii(encoding='hex'))
+print("Public Key (32 bytes):", pubkey.to_ascii(encoding='hex'))
+
+# Signing a message.
+msg = "Sign this message."
+signature = privkey.sign(msg, encoding='hex')
+
+# Verification of signed message.
+try:
+  pubkey.verify(signature, msg, encoding='hex')
+  print("The signature is valid.")
+except:
+  print("The signature is invalid.")
