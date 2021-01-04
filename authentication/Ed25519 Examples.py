787f09a46d0063fc6b9b859ef95ad4b9902e776a
Roger Bai <69400368+roger-bai@users.noreply.github.com>
2021-01-04 18:01:18 -0500

    Update Ed25519 Examples.py

--- authentication/Ed25519 Examples.py
+++ authentication/Ed25519 Examples.py
@@ -10,7 +10,7 @@ print("Private Key (32 bytes):", privkey.to_ascii(encoding='hex'))
 print("Public Key (32 bytes):", pubkey.to_ascii(encoding='hex'))
 
 # Signing a message.
-msg = "Sign this message."
+msg = b"Sign this message."
 signature = privkey.sign(msg, encoding='hex')
 
 # Verification of signed message.
