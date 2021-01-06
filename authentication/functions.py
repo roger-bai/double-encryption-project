5c9ba68d98a844f73a7d07f15d9dd350912fe53f
Roger Bai <69400368+roger-bai@users.noreply.github.com>
2021-01-06 11:34:35 -0500

    Update functions.py

--- authentication/functions.py
+++ authentication/functions.py
@@ -2,25 +2,25 @@ import sys
 import subprocess
 
 # implement pip as a subprocess to install packages:
-for package in ['ed25519']:
+for package in ['ed25519', 'ocsp-checker']:
   subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
   
 import ed25519
+from ocspchecker import ocspchecker
 
-def sign_message(msg):
-  """ (bytes) -> bytes, VerifyingKey
-  
-  Creates a public/private key pair and signs the msg using the private key. 
-  Returns the signed msg and the public key to verify the signature."""
+def sign_message(msg: bytes):
+  """ 
+  Returns a public/private key pair and signs the msg using the private key. 
+  Returns the signed msg and the public key to verify the signature.
+  """
   
   # msg needs to be in bytes.
   privkey, pubkey = ed25519.create_keypair()
   signature = privkey.sign(msg, encoding='hex')
   return signature, pubkey
   
-def verify_signature(msg, signature, pubkey):
-  """(bytes, bytes, VerifyingKey) -> bool
-  
+def verify_signature(msg: bytes, signature: bytes, pubkey) -> bool:
+  """
   Uses the pubkey to check that the signed msg is signature. If so, returns True, else False.
   """
   
@@ -29,3 +29,10 @@ def verify_signature(msg, signature, pubkey):
     print(True)
   except:
     print(False)
+
+def ocsp_check(server: str) -> str:
+  """
+  Takes a website and checks their certificate status using OCSP.
+  """
+  
+  return ocspchecker.get_ocsp_status(server)[-1]
