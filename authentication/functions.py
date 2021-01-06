d1fbfb80848f30af0384b75b21ef842290f40127
Roger Bai <69400368+roger-bai@users.noreply.github.com>
2021-01-05 19:12:20 -0500

    Create functions.py

---
+++ authentication/functions.py
@@ -0,0 +1,31 @@
+import sys
+import subprocess
+
+# implement pip as a subprocess to install packages:
+for package in ['ed25519']:
+  subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+  
+import ed25519
+
+def sign_message(msg):
+  """ (bytes) -> bytes, VerifyingKey
+  
+  Creates a public/private key pair and signs the msg using the private key. 
+  Returns the signed msg and the public key to verify the signature."""
+  
+  # msg needs to be in bytes.
+  privkey, pubkey = ed25519.create_keypair()
+  signature = privkey.sign(msg, encoding='hex')
+  return signature, pubkey
+  
+def verify_signature(msg, signature, pubkey):
+  """(bytes, bytes, VerifyingKey) -> bool
+  
+  Uses the pubkey to check that the signed msg is signature. If so, returns True, else False.
+  """
+  
+  try:
+    pubkey.verify(signature, msg, encoding='hex')
+    print(True)
+  except:
+    print(False)
