0d980692d0c4df9e98dafbd893c00a0093f4be63
Roger Bai <69400368+roger-bai@users.noreply.github.com>
2021-01-16 13:00:33 -0500

    Update cloud_decrypt.py

--- gcp/cloud_decrypt.py
+++ gcp/cloud_decrypt.py
@@ -1,1 +1,31 @@
+"""
+Script for cloud VM to decrypt a csv file.
+"""
 
+import encryption
+import os
+import vault
+import verification
+
+test_file = FILE_TO_DECRYPT
+
+# Verify Signature
+pubkey = PUBLIC_KEY_LOCATION
+key_file = DEKS_OF_FILE_TO_DECRYPT
+signature = SIGNATURE_LOCATION
+verification.verify_sig(pubkey, key_file, signature)
+
+os.remove(pubkey) # Best to use srm or equivalent
+os.remove(signature)
+
+# Use DEK to decrypt file
+DEKs = {}
+with open(key_file) as f:
+    for line in f:
+        (key, val) = line.split()
+        DEKs[int(key)] = vault.decode_base64(val).encode('latin-1')
+
+encryption.decrypt_CSV(test_file, DEKs)
+
+# Delete DEKs (best to use srm)
+os.remove(key_file)
