47fe63b76023ed8723524f9c3483952c8935afbd
Roger Bai <69400368+roger-bai@users.noreply.github.com>
2021-01-16 12:43:02 -0500

    Update upload_storage.py

--- gcp/upload_storage.py
+++ gcp/upload_storage.py
@@ -1,1 +1,34 @@
+import ocsp
+import vault
+import google_func
+import encryption
 
+# OCSP check
+hostname = 'console.cloud.google.com'
+ocsp_status = ocsp.ocsp_check(hostname)
+if ocsp_status != 'OCSP Status: GOOD':
+    print(ocsp_status)
+    quit()
+
+# The file directory and columns to encrypt
+testfile = TEST_FILE_LOCATION
+cols = COLUMNS_OF_TEST_FILE_TO_ENCRYPT
+
+# DEKs and replace file with encrypted file
+DEKs = encryption.generate_DEKs(cols)
+encryption.encrypt_CSV(testfile, DEKs)
+
+# Encrypt DEKs
+# Sometimes fails depending on the 'latin-1' bytes.
+encrypted_DEKs = {}
+for k in DEKs:
+    encrypted_DEKs[k] = vault.encrypt('KEK',
+                        vault.encode_base64(DEKs[k].decode('latin-1')))
+
+# Create file with encrypted DEKs
+with open(testfile + '.txt', 'w') as f:
+    for k in encrypted_DEKs:
+        f.write(str(k) + '\t ' + encrypted_DEKs[k].decode('utf-8') + "\n")
+
+# Upload to storage
+google_func.upload_files(google_func.bucketName)
